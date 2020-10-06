9743b26d8155fdcf3f1fc404a5d7cd05ae54678c
Nash-BEAT <nash.0cal@gmail.com>
2020-10-07 00:32:41 +0900

    first

---
+++ test.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+
+iris = datasets.load_iris()
+iris_data = iris.data
+sl_data = iris_data[:100, 0] # SetosaとVersicolor、Sepal length
+sw_data = iris_data[:100, 1] # SetosaとVersicolor、Sepal width
+
+print(iris_data)
